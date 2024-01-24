8498b6240950b008dc7a0320bee75d845e636403
Sangya Sherpa <135448616+13x54n@users.noreply.github.com>
2024-01-24 07:01:28 +0000

    Working on checkout page

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,7 +3,7 @@ import { Dialog, Transition } from "@headlessui/react";
 import { XMarkIcon } from "@heroicons/react/24/outline";
 import { userSignOut } from "../utils/authentication";
 
-export default function Sidebar({user}) {
+export default function Sidebar({ user }) {
   const [sidebarOpen, setSidebarOpen] = useState(false);
   return (
     <>
@@ -73,14 +73,42 @@ export default function Sidebar({user}) {
                         </button>
                       </div>
                     </Transition.Child>
-                    <div className="flex h-full flex-col overflow-y-scroll bg-white py-6 shadow-xl">
-                      <div className="px-4 sm:px-6">
-                        <Dialog.Title className="text-base font-semibold leading-6 text-gray-900">
+                    <div className="px-4 sm:px-6 py-6 flex h-full flex-col overflow-y-scroll bg-white shadow-xl">
+                      <div className="border-b-2 pb-2">
+                        <Dialog.Title className="text-base leading-6 text-gray-900">
+                          <p className="text-base font-semibold">
+                            Hey, {user.email.split("@")[0]}!
+                          </p>
                           {user.email}
                         </Dialog.Title>
                       </div>
-                      <div className="relative mt-6 flex-1 px-4 sm:px-6">
-                        <button onClick={() => userSignOut()}>Sign Out</button>
+                      <div className="flex items-center gap-2 mt-3">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 24 24"
+                          fill="currentColor"
+                          className="w-4 h-4"
+                        >
+                          <path d="M2.25 2.25a.75.75 0 0 0 0 1.5h1.386c.17 0 .318.114.362.278l2.558 9.592a3.752 3.752 0 0 0-2.806 3.63c0 .414.336.75.75.75h15.75a.75.75 0 0 0 0-1.5H5.378A2.25 2.25 0 0 1 7.5 15h11.218a.75.75 0 0 0 .674-.421 60.358 60.358 0 0 0 2.96-7.228.75.75 0 0 0-.525-.965A60.864 60.864 0 0 0 5.68 4.509l-.232-.867A1.875 1.875 0 0 0 3.636 2.25H2.25ZM3.75 20.25a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0ZM16.5 20.25a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0Z" />
+                        </svg>
+                        <button>Orders</button>
+                      </div>
+                      <div className="relative flex-1">
+                        <button className="flex items-center gap-2" onClick={() => userSignOut()}>
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 24 24"
+                            fill="currentColor"
+                            className="w-4 h-4"
+                          >
+                            <path
+                              fillRule="evenodd"
+                              d="M16.5 3.75a1.5 1.5 0 0 1 1.5 1.5v13.5a1.5 1.5 0 0 1-1.5 1.5h-6a1.5 1.5 0 0 1-1.5-1.5V15a.75.75 0 0 0-1.5 0v3.75a3 3 0 0 0 3 3h6a3 3 0 0 0 3-3V5.25a3 3 0 0 0-3-3h-6a3 3 0 0 0-3 3V9A.75.75 0 1 0 9 9V5.25a1.5 1.5 0 0 1 1.5-1.5h6ZM5.78 8.47a.75.75 0 0 0-1.06 0l-3 3a.75.75 0 0 0 0 1.06l3 3a.75.75 0 0 0 1.06-1.06l-1.72-1.72H15a.75.75 0 0 0 0-1.5H4.06l1.72-1.72a.75.75 0 0 0 0-1.06Z"
+                              clipRule="evenodd"
+                            />
+                          </svg>
+                          Sign Out
+                        </button>
                       </div>
                     </div>
                   </Dialog.Panel>
